17b3c60d82f09f9755b8445d873afc3609127b2b
hg.kang <kin0204@naver.com>
2025-12-08 23:27:28 +0900

    ✨ feat : logback을 활용한 로그 기록(#6)
    logback을 활용 하여 로그 기록
    - 현재 파일(당일) 로그 파일 경로 ./logs/current/로그타입별명칭.log
    - 과거 로그 파일 경로 ./logs/yyyy/MM/requests/yyyy-mm-dd-로그레벨.log
    응답 지연 별도 로그파일 저장(현재 1초 이상 지연 저장하게 설정)
    - 과거 로그 파일 경로 ./logs/yyyy/MM/performance_warn/yyyy-mm-dd-per_warn.log
    
    feat : #6

--- src/main/java/org/ateam/oncare/common/ApplicationController.java
+++ src/main/java/org/ateam/oncare/common/ApplicationController.java
@@ -1,13 +1,30 @@
 package org.ateam.oncare.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+
     @GetMapping("/health")
     public String health() {
+        return "I'm OK";
+    }
+
+    @GetMapping("/error1")
+    public String error() {
+        try{
+            logger.debug("error 호출");
+            logger.warn("곧 에러발생 예정");
+            Thread.sleep(1000);
+            throw new Exception("I'm Error");
+        } catch(Exception e){
+            logger.error(e.getMessage());
+        }
         return "I'm OK";
     }
 }
