311283452a3062f918dbe36125ada66e41b9450f
seoguhoe2 <js8337349@naver.com>
2025-12-16 10:59:24 +0900

    Request: 고객 타입(잠재/기존), 페이지네이션 정보 Response: 상담 이력, 해당 상담이 진행된 날짜, 시간, 상담사 이름, 상담 세부사항
    이슈번호 #35

--- src/main/java/org/ateam/oncare/counsel/query/controller/CounselQueryController.java
+++ src/main/java/org/ateam/oncare/counsel/query/controller/CounselQueryController.java
@@ -4,16 +4,20 @@ import org.ateam.oncare.counsel.query.dto.CounselListResponse;
 import org.ateam.oncare.counsel.query.dto.CustomerListResponse;
 import org.ateam.oncare.counsel.query.service.CounselQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/counsel")
+@Validated
 public class CounselQueryController {
     private final CounselQueryService counselQueryService;
 
@@ -26,6 +30,15 @@ public class CounselQueryController {
     public ResponseEntity<List<CustomerListResponse>> requestCustomerList(
             @RequestParam(value = "keyword", required = false)String keyword) {
         return ResponseEntity.ok(counselQueryService.searchCustomers(keyword));
+    }
+
+    @GetMapping("/customers/{customerId}/counsels")
+    public ResponseEntity<Slice<CounselListResponse>> requestCounselList(
+            @PathVariable("customerId") BigInteger customerId,
+            @RequestParam("customerType") String customerType,
+            @PageableDefault(page = 0, size = 5, sort = "consult_date", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        return ResponseEntity.ok(counselQueryService.findCounselsByCustomerId(customerId, customerType, pageable));
     }
 
 }
--- src/main/java/org/ateam/oncare/counsel/query/mapper/CounselQueryMapper.java
+++ src/main/java/org/ateam/oncare/counsel/query/mapper/CounselQueryMapper.java
@@ -1,10 +1,12 @@
 package org.ateam.oncare.counsel.query.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.ateam.oncare.counsel.query.dto.CounselListResponse;
 import org.ateam.oncare.counsel.query.dto.CustomerListResponse;
 import org.jspecify.annotations.Nullable;
 
+import java.math.BigInteger;
 import java.util.List;
 
 @Mapper
@@ -14,4 +16,8 @@ public interface CounselQueryMapper {
     @Nullable List<CustomerListResponse> searchCustomersByPhone(String keyword);
 
     @Nullable List<CustomerListResponse> searchCustomersByName(String keyword);
+
+    List<CounselListResponse> findCounselsByCustomerId(@Param("customerId")BigInteger customerId,
+                                                       @Param("customerType")String customerType,
+                                                       @Param("limit")int limit, @Param("offset")long offset);
 }
